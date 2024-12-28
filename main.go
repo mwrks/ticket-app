87a4e9a4d66e59f10e8e1d39a0998f4404739a65
Aizu <mfaiz2035@gmail.com>
2024-12-28 23:50:44 +0700

    v1

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"ticket-app/initializers"
+	"ticket-app/routes"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectDatabase()
+}
+func main() {
+	r := routes.SetupRouter()
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
---
+++ migrate/migrate.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"ticket-app/initializers"
+	"ticket-app/models"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectDatabase()
+}
+
+func main() {
+	err := initializers.DB.AutoMigrate(&models.Ticket{}, &models.Order{})
+	if err != nil {
+		panic(fmt.Sprintf("Failed to migrate database: %v", err))
+	}
+}
